8e6216304298bea56dbe1a926a3cc24e2457876d
Howard Arca <howardarca55@gmail.com>
2026-08-26 14:19:34 +0800

    Update config.py

--- WOW-DING-TALAGA-XI-CED/backend/config.py
+++ WOW-DING-TALAGA-XI-CED/backend/config.py
@@ -2,7 +2,7 @@ SECRET_KEY = "dhing_xi_ced_secret_6969"
  
 DB_HOST     = "localhost"
 DB_USER     = "root"
-DB_PASSWORD = "108388"
+DB_PASSWORD = "secret"
 DB_NAME     = "ecommerce"
 MAIL_USERNAME = "grapxoptum@gmail.com"
 MAIL_PASSWORD = "wfziotnlmpvwfudz"
